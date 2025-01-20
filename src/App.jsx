5f0f85f3a2f2a1ebc7f562e401bb6326c6ee275e
munnaahmed <web.munnaahmed@gmail.com>
2025-01-20 03:00:23 -0800

    folder structure done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import { useState } from "react"
+import NewsList from "./components/body/NewsList"
 
 function App() {
-
+  
   return (
     <>
      <h1 className="text-blue-400">home</h1>
      <h2>Updated by Munna</h2>
+     <NewsList />
     </>
   )
 }
