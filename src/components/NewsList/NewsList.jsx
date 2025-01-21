89a9e8355d4167551f3b6ca4f91452cde97ea886
Ramjan <mdramjanali@gmail.com>
2025-01-21 10:32:55 +0600

    updated

--- src/components/NewsList/NewsList.jsx
+++ src/components/NewsList/NewsList.jsx
@@ -1,45 +1,39 @@
-import axios from "axios"
-import { useEffect, useState } from "react"
-
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const NewsList = () => {
-    const [newsList, setNewsList] = useState([])
+  const [newsList, setNewsList] = useState([]);
 
-    const fetchNewses = async () =>{
-        try {
-            const response = await axios.get('https://api.pewds.vercel.app/prothomalo/home/');
-            console.log(response.data)
-            setNewsList(response.data)
-        } catch (error) {
-            console.log(error)
-        }
+  const fetchNewses = async () => {
+    try {
+      const response = await axios.get(
+        "https://api.pewds.vercel.app/prothomalo/home/"
+      );
+      console.log(response.data);
+      setNewsList(response.data);
+    } catch (error) {
+      console.log(error);
     }
+  };
 
+  useEffect(() => {
+    fetchNewses();
+  }, []);
 
-    useEffect(() => {
-        fetchNewses()
-    }, [])
-    
   return (
     <div>
       <h2>{newsList?.customSeo?.title}</h2>
 
-      {newsList?.collection?.items.map((item, i)=>{
+      {newsList?.collection?.items.map((item, i) => {
         return (
-<<<<<<< HEAD:src/components/body/NewsList.jsx
-            <div>
-                {item?.name}
-                {item?.summary}
-=======
-            <div key={i}>
-                <h1>{item?.name}</h1>
-                <p>{item?.summary}</p>
->>>>>>> 896de9579792194cdb970d15e499431e001ee6f5:src/components/NewsList/NewsList.jsx
-            </div>
-        )
+          <div key={i}>
+            <h1>{item?.name}</h1>
+            <p>{item?.summary}</p>
+          </div>
+        );
       })}
     </div>
-  )
-}
+  );
+};
 
-export default NewsList
+export default NewsList;
