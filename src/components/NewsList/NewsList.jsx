840403eaab7e5934dd7ac3ab9773d9c4bb779962
SABBIR657 <mdsabbirrahman2025@gmail.com>
2025-01-20 20:56:20 -0800

    file deleted and updated

--- src/components/NewsList/NewsList.jsx
+++ src/components/NewsList/NewsList.jsx
@@ -26,15 +26,9 @@ const NewsList = () => {
 
       {newsList?.collection?.items.map((item, i)=>{
         return (
-<<<<<<< HEAD:src/components/body/NewsList.jsx
-            <div>
-                {item?.name}
-                {item?.summary}
-=======
             <div key={i}>
                 <h1>{item?.name}</h1>
                 <p>{item?.summary}</p>
->>>>>>> 896de9579792194cdb970d15e499431e001ee6f5:src/components/NewsList/NewsList.jsx
             </div>
         )
       })}
